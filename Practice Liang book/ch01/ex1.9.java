e7d7fd668e7c51a5ded9c20aa02060fae42e3a9a
Md Shishir Kaysar Shuvon <148075201+shuv-on@users.noreply.github.com>
2024-11-16 19:47:23 +0600

    ex1.9.java

---
+++ Practice Liang book/ch01/ex1.9.java
@@ -0,0 +1,8 @@
+
+class Main {
+    public static void main(String[] args) {
+       double a=4.5,b=7.9;
+       System.out.println("Perimeter is : "+ (2*(a+b)));
+       System.out.println("Area is : "+(a*b));
+    }
+}
