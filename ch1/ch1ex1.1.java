dc98db211211fd79e572597cf004d28bedc97427
Md Shishir Kaysar Shuvon <148075201+shuv-on@users.noreply.github.com>
2024-11-16 19:20:14 +0600

    ch1ex1.1.java

---
+++ ch1/ch1ex1.1.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java");
+        System.out.println("Welcome To ICT");
+        System.out.println("Programming is fun!");
+    }
+}
