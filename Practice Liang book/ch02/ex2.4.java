e33579045d1a4d881fc3e001ccec89e41823876f
Md Shishir Kaysar Shuvon <148075201+shuv-on@users.noreply.github.com>
2024-11-18 21:50:49 +0600

    ex2.4.java

---
+++ Practice Liang book/ch02/ex2.4.java
@@ -0,0 +1,9 @@
+//pounds to kilograms
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double pound=input.nextDouble();
+        System.out.println(pound+ " Pound  = " + pound/0.454 + "Kilograms");
+    }
+}
