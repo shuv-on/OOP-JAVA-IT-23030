ccea9db291517cb6b90b2babe63c09ae757b6db6
Md Shishir Kaysar Shuvon <148075201+shuv-on@users.noreply.github.com>
2024-11-18 22:06:09 +0600

    ex2.7.java

--- Practice Liang book/ch02/ex2.7.java
+++ Practice Liang book/ch02/ex2.7.java
@@ -1,1 +1,12 @@
-
+//Find the numbers of year
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int minit;
+        minit=input.nextInt();
+        int year=minit/(365*24*60);
+        int day=(minit-year*365*24*60)/(24*60);
+        System.out.println(minit+"minutes is approximately "+year+" years and "+day+" days");
+    }
+}
